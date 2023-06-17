63ef035bbdf75eac60bfa2a5eba0a3312b33034d
Jordan Olguin <jordanolguin@iMac.local>
2023-06-16 20:04:35 -0600

    app.get to display notes up

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+// app.get("/", (req, res) => res.send());
 
-app.get("/send", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/send.html"))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("/paths", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/paths.html"))
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./db/db.json"))
 );
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+        console.log(req.body);
+      const parsedData = JSON.parse(data);
+    //   parsedData.push(content);
+    //   writeToFile(file, parsedData);
+    }
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
